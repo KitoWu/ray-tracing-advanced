cc956ec939a5041046118f7b109fc800074742ea
吴启通 <macwu@Macbook.local>
2025-08-05 11:44:47 +0800

    完善功能

--- rtweekend.h
+++ rtweekend.h
@@ -2,10 +2,12 @@
 #define RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <memory>
 #include <limits>
 #include <vector>
+#include <random>
 
 using std::shared_ptr;
 using std::make_shared;
@@ -15,6 +17,16 @@ const double infinity = std::numeric_limits<double>::infinity();
 
 inline double degrees_to_radians(double degrees){
     return degrees * pi / 180.0;
+}
+
+inline double random_double(){
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max){
+    return min + (max - min) * random_double();
 }
 
 #include "interval.h"
